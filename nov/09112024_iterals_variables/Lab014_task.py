36dcef02b620426b6435ca3e256f933df0c65aea
kumar <kumaritulika873@gmail.com>
2024-11-12 08:16:55 +0530

    task 2 9th nov

---
+++ nov/09112024_iterals_variables/Lab014_task.py
@@ -0,0 +1,11 @@
+# Task 1 for the Today
+# Take a 2 input from the user
+# Print the Quotient and Remainder
+num1 = int(input("enter the num 1"))
+num2 = int(input("enter the num 2"))
+
+quotient = num1//num2
+reminder = num1%num2
+
+print("quotient is : ", quotient)
+print("reminder is : ", reminder)
