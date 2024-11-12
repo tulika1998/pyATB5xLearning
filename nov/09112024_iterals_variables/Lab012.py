60afd0737d0faf62b89d966ded29ec2d7ed1432d
kumar <kumaritulika873@gmail.com>
2024-11-12 08:10:08 +0530

    task 9th nov

---
+++ nov/09112024_iterals_variables/Lab012.py
@@ -0,0 +1,10 @@
+name = "tulika"
+print(len(name))
+print(name.upper())
+print(name.lower())
+
+name = name +str(1)
+print(name)
+
+print("Hello \n World")
+print("Hello \t World")
---
+++ nov/09112024_iterals_variables/Lab013_tasks.py
@@ -0,0 +1,23 @@
+# Task for the Today
+# Take a 3 input from the user
+# perform the sub, sub, mul and div
+
+num1 = input("enter the num 1")
+num2 = input("enter the num 2")
+num3 = input("enter the num 3")
+
+print(type(num1))
+
+num1 = int(input("enter the num 1"))
+num2 = int(input("enter the num 2"))
+num3 = int(input("enter the num 3"))
+
+sum = num1+num2+num3
+sub = num1-num2-num3
+mul = num1*num2*num3
+div = num1/num2
+
+print("sum is : ", sum)
+print("sub is : ", sub)
+print("mul is : ", mul)
+print("div is : ", div)
