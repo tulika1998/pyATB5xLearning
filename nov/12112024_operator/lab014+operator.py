1f58b68e8df4de1292f947bb9b7932cdb068e028
kumar <kumaritulika873@gmail.com>
2024-11-14 09:47:15 +0530

    12th nov

---
+++ nov/12112024_operator/lab014+operator.py
@@ -0,0 +1,10 @@
+#age = 65
+#age = ref variable name/ identifier
+# = operator
+# 65 = variable value -- literal
+
+#unary operator -   which is added to operand
+age = +65
+age2 = -65
+print(age)
+print(age2)
---
+++ nov/12112024_operator/lab015_operator_arthmetic.py
@@ -0,0 +1,15 @@
+#// ---quotient
+#% -- reminder
+#**-- power
+# ==   compare operator (true or false)
+# not operator
+
+
+print(13//2)
+print(13%2)
+print(2**2)
+print(2 == 2)
+print(2 == 3)
+is_tcs_poor = False
+print(not is_tcs_poor)
+print(is_tcs_poor)
