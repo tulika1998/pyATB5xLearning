733cdc93622f7c914129b4f3dcd9df021a82eeec
kumar <kumaritulika873@gmail.com>
2024-11-14 11:25:44 +0530

    12th nov

---
+++ nov/12112024_operator/LAB016_OPERATOR.py
@@ -0,0 +1,18 @@
+#logical operator --- bool
+x = 10
+y = 20
+print (x >y)
+print (x <y)
+
+a = 10
+b = 10
+print(a>=b)
+
+#increment (++)  / decrement (--)
+x = 6
+x+=1
+print(x)
+x-=1
+print(x)
+x*=2
+print(x)
---
+++ nov/12112024_operator/lab017_ternary operator.py
@@ -0,0 +1,15 @@
+#if else operator
+x = 10
+y = 20
+print("x is greater" if x>y else "y is greater")
+
+#if x > y:
+#print("x is greater")
+#else:
+#print("y is greater")
+
+user_age = int(input("enter your age\n"))
+if user_age>18:
+  print("you can go ranchi and vote")
+else:
+    print("no you canot go ranchi and vote")
