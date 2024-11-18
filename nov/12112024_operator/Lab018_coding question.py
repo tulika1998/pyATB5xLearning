7f3f25552664d4f9d4e1b70afdeb399430f4218c
kumar <kumaritulika873@gmail.com>
2024-11-18 08:21:12 +0530

    task1

---
+++ nov/12112024_operator/Lab018_coding question.py
@@ -0,0 +1,6 @@
+#calculate the area of circle
+radius = float(input("Enter the value of radius of the circle \n"))
+print(radius)
+area = 3.14 * (radius**2)
+print("area of the circle ->: ", area)
+print(f"area of the circle is (area) : {area:.3f}")
---
+++ nov/12112024_operator/Lab019_task1.py
@@ -0,0 +1,6 @@
+#Write a program to take a user age and let him know if he can go the club.  21
+user_age = int(input("enter your age\n"))
+if user_age>=21:
+  print("you can go to the club")
+else:
+    print("no you canot go to the club")
