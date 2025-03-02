4a08cb61986c0048bf12d35cb096eb2c2442c33d
kumar <kumaritulika873@gmail.com>
2025-03-02 16:02:49 +0530

    01032025

--- nov/09112024_iterals_variables/Lab012.py
+++ nov/09112024_iterals_variables/Lab012.py
@@ -3,8 +3,19 @@ print(len(name))
 print(name.upper())
 print(name.lower())
 
-name = name +str(1)
+age = "30"
+age1 = 30
+print(type(age))
+print(type(age1))
+
+name = name +str(1) #can only concatenate str (not "int") to str
 print(name)
 
-print("Hello \n World")
-print("Hello \t World")
+print("Hello\nWorld")
+print("Hello\tWorld")
+print("Hello\bworld")
+
+#raw string concept
+dir = 'C:\tulika\n.txt'
+dir = r'C:\tulika\n.txt'
+print(dir)
