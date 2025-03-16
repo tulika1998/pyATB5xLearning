9e6fedae59796849b212e857d7a695cbfae73b79
kumar <kumaritulika873@gmail.com>
2025-03-16 22:50:01 +0530

    10032025

--- nov/23112024/Lab047_lambda.py
+++ nov/23112024/Lab047_lambda.py
@@ -7,6 +7,8 @@
 
 find_even_odd(17)'''
 
+n = int(input("enter a number\n"))
 check_even_odd = lambda num: "even" if num % 2 == 0 else "odd"
-print(check_even_odd(18))
-print(check_even_odd(15))
+#print(check_even_odd(18))
+#print(check_even_odd(15))
+print(check_even_odd(n))
---
+++ nov/23112024/Lab051.py
@@ -0,0 +1,4 @@
+def sum_three_num(a, b, c):
+    return a+b+c
+op = lambda a, b, c: a+b+c
+print(op(358,85,75))
